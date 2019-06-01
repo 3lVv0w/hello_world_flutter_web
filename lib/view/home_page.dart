10561d0abb0cfaf6744df8675ee3fa33658bbe60
3lVv0w <krseisenh@gmail.com>
2019-06-01 10:40:10 +0700

    typo

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
               'Google 1/0 2019!',
             ),
             Text(
-              'Exntendded BKK',
+              'Extended BKK',
             ),
             Text(
               'ภาษาไทยรองรับหรือยัง',
